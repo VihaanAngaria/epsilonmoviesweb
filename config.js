f1e29898e863008c54f2da4d0254dc0347a41785
VihaanAngaria <vihaanangaria@gmail.com>
2026-07-18 22:47:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.EPSILON_CONFIG = {
   siteUrl: 'https://epsilonmovies.space-z.ai',
-  f1Url: 'https://epsilonf1tv.space-z.ai',
+  f1Url: 'https://epsilonf1.space-z.ai',
   telegramUrl: 'https://t.me/epsilonmovies',
   instagramUrl: 'https://instagram.com/epsilonmovies.in',
-  contactEmail: 'ownerepsilon@zohomail.in',
+  contactEmail: 'support@epsilonmovies.in',
   futureDomains: ['https://epsilonmovies.in']
 };
